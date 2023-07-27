789cb871a4ccd6da240b3c74e404e92ac4badec2
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-07-27 10:53:16 +0530

    expressStarted

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const http = require('http');
-const route  = require('./routes.js');
-const server = http.createServer(route);
-server.listen(3000);
+const express  = require('express');
+const app = express();
+const server = http.createServer(app);
+app.use((req, res, next) => {
+    console.log('Hello from Middleware');
+    next();
+})
+app.use((req, res, next) => {
+    console.log('Hello from another Middleware');
+})
+app.listen(3000);
