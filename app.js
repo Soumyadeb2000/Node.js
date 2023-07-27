4c316ec4a924a1bffad5954fcaee819927b40e1c
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-07-27 16:14:40 +0530

    introToMiddleware

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
-const express  = require('express');
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-app.use((req, res, next) => {
-    console.log('Hello from Middleware');
-    next();
-})
-app.use((req, res, next) => {
-    console.log('Hello from another Middleware');
-})
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add</button></form>')
+});
+
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Hello from Server</h1>');
+});
+
 app.listen(3000);
