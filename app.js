ffed90261df4456d5f53ea84c6df4ff546f9b5de
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-07-30 18:16:24 +0530

    styling added

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const http = require('http');
-
+const path = require('path');
 const express = require('express');
-
 const adminRoute = require('./Routes/admin')
-
-const shopRoute = require('./Routes/shop')
+const shopRoute = require('./Routes/shop');
 const app = express();
 const bodyparser = require('body-parser');
+const rootDir = require('./util/path.js');
 app.use(bodyparser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoute);
 
-app.use('/shop', shopRoute);
+app.use('/', shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send("Page not found");
+    res.status(404);
+    res.sendFile(path.join(rootDir, 'views', 'error.html'));
 })
 
 app.listen(3000);
