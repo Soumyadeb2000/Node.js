ae96e24cbe59e64d0fa02ef694903bf4c1478a15
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-07-30 18:19:19 +0530

    npm and nodemon

--- Chat app/home.js
+++ Chat app/home.js
@@ -1,25 +1,18 @@
 const express = require('express');
-const fs = require('fs')
 const router = express.Router();
 const bodyparser = require('body-parser');
-const data = [];
-router.use(bodyparser.urlencoded({extended: false}))
+const fs = require('fs');
+
+router.use(bodyparser.urlencoded({extended: false}));
 
 router.get('/', (req, res, next) => {
-    let message = fs.readFileSync('./Chat app/message.txt', 'utf8');
-    if(message === 'undefined: undefined'){
-    res.send(`<body>No Message<br><form onsubmit="getElementById('username').value = localStorage.getItem('loginData')" action="/" method="POST"><input type="text" name="chatData"><input type="hidden" name="username" id="username"><br><button type="submit">Send</button></form></body>`);
-    message="";
-    }
-    res.send(`<body>${message} <br><form onsubmit="getElementById('username').value = localStorage.getItem('loginData')" action="/" method="POST"><input type="text" name="chatData"><input type="hidden" name="username" id="username"><br><button type="submit">Send</button></form></body>`);
+    res.send(`${fs.readFileSync('chat.txt', 'utf8')} <br> <form onsubmit="document.getElementById('name').value = localStorage.getItem('username')" action="/" method="POST"><input type="text" name="message" id"message"><input type="hidden" name="name" id="name"><button type="submit">Send</button></form>`)
 })
 
 router.post('/', (req, res, next) => {
-    if(req.body.chatData != undefined && req.body.chatData != null){
-    data.push(`${req.body.username}: ${req.body.chatData}`);
-    fs.writeFileSync('./Chat app/message.txt', data.toString())
-    }
+    if(req.body.name != undefined || req.body.message != undefined)
+    fs.appendFileSync('chat.txt',`${req.body.name}: ${req.body.message} `);
     res.redirect('/');
-})
+}) 
 
 module.exports = router;
--- Chat app/login.js
+++ Chat app/login.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res, next) => {
-    res.send('<form onsubmit="localStorage.setItem(`loginData`, document.getElementById(`loginData`).value)" action="/" method="POST"><input type="text" id="loginData" name="loginData"><br><button type="submit">Login</button></form>')
+    res.send('<BODY><FORM action="/" method="POST" onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)"><input type="text" name="username" id="username"><button type="submit">Login</button></FORM></BODY>');
 })
-
 
 module.exports = router;
