25993dbd1d5ddecc2638887dda2dab8b728804ef
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-07-31 22:55:13 +0530

    modelsTask

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const http = require('http');
 const path = require('path');
+
 const express = require('express');
-const adminRoute = require('./Routes/admin')
-const shopRoute = require('./Routes/shop');
-const contactRoute = require('./Routes/contact');
+const bodyParser = require('body-parser');
+
 const app = express();
-const bodyparser = require('body-parser');
-const rootDir = require('./Util/path.js');
-app.use(bodyparser.urlencoded({extended: false}));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error'); 
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', contactRoute)
-app.use('/admin', adminRoute);
-app.use('/', shopRoute);
-app.use((req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'success-page.html'))
-})
-app.use((req, res, next) => {
-    res.status(404);
-    res.sendFile(path.join(rootDir, 'views', 'error.html'));
-})
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.getError);
+
 app.listen(3000);
