a763acfb543dac782652132bc59c486540cfad0b
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-08-01 15:28:58 +0530

    Dynamic Routes Part 1

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,14 +1,18 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
-const rootDir = require('../Util/path.js');
-router.get('/add-product',(req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
 
-router.post('/add-product',(req,res,next) => {
-    res.redirect('/');
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
