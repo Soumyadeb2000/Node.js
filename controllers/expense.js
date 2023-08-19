a655c3f6302a9ff732a9546b48c38009fd5c6f92
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-08-19 14:15:16 +0530

    Transaction added

--- controllers/expense.js
+++ controllers/expense.js
@@ -2,43 +2,43 @@ const Expense = require('../models/expense');
 
 const User = require('../models/user');
 
+const sequelize = require('../utils/database');
+
 exports.deleteExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();
     try {
         const eid = req.params.id;
         if(eid === 'undefined' || eid === 'null' || eid === '') {
             console.log("Invalid id");
             return res.status(500).json({err: "Invalid id"});
         }
-        
         const expenseToBeDeleted = await Expense.findAll({where: {id: eid, userId: req.user.id}})
         const expenseAmountToBeDeleted = Number(expenseToBeDeleted[0].amount);
-        console.log(expenseAmountToBeDeleted);
         const users = await User.findAll({where: {id: req.user.id}});
-        const user = users[0];
-        const updatedTotalExpense = Number(user.totalExpense) - expenseAmountToBeDeleted;
-        console.log(updatedTotalExpense);
-        await user.update({totalExpense: updatedTotalExpense})
-        await Expense.destroy({where: {id: eid, userId: req.user.id}});
-        return res.status(201).json({res: "Deletion successful", asn: expenseToBeDeleted});
+        const updatedTotalExpense = Number(users[0].totalExpense) - expenseAmountToBeDeleted;
+        await users[0].update({totalExpense: updatedTotalExpense}, {transaction: t})
+        await Expense.destroy({where: {id: eid, userId: req.user.id}, transaction: t});
+        await t.commit();
+        return res.status(201).json({res: "Deletion successful"});
         
     } catch (error) {
-        console.log(("Something went wrong!"));
+        await t.rollback();
         return res.status(500).json({err: "Something went wrong!"})
     }
 }    
 
-exports.getExpenses = (req, res, next) => {
-    Expense.findAll({where: {userId: req.user.id}})
-    .then(expense => {
-        return res.status(200).json({expenses: expense});
-    })
-    .catch(err => {
-        console.log(err);
+exports.getExpenses = async (req, res, next) => {
+    try {
+        const expenses = await Expense.findAll({where: {userId: req.user.id}})
+        return res.status(200).json({expenses: expenses});
+    } catch (error) {
         return res.status(404).json({error: "Data not found!"})
-    })
+    }
+
 }
 
 exports.postExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();
     try {
         const amount = req.body.amount;
         const category = req.body.category;
@@ -48,11 +48,12 @@ exports.postExpense = async (req, res, next) => {
         const currentTotalExpense = Number(user.totalExpense) + Number(amount);
             await user.update({
                 totalExpense: currentTotalExpense
-            });
-        const data = await Expense.create({amount: amount, category: category, description: description, userId: req.user.id});
+            }, {transaction: t});
+        const data = await Expense.create({amount: amount, category: category, description: description, userId: req.user.id}, {transaction: t});
+        await t.commit();
         return res.status(200).json({newExpenseData: data});    
     } catch (error) {
-        console.log(error);
+        await t.rollback();
         return res.status(500).json({Error: "Something went wrong"})
     }
 
