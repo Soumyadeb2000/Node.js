d4f87b254a0e040455dca9201ecb23f984c3d30f
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-10-04 02:11:30 +0530

    MongoDB assignment 1

---
+++ mongoDB practice/models/product.js
@@ -0,0 +1,26 @@
+const getDb = require('../util/database').getDB;
+
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  
+  save() {
+    const db = getDb();
+    return db.collection('products')
+    .insertOne(this)
+    .then(res => {    
+      // console.log(this);
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+}
+
+module.exports = Product;
